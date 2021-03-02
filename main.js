fe4290a1dfab1267ab6a8686f6504cd0d5469613
Oddidude <luke.cheungster@gmail.com>
2021-03-02 22:50:04 +0000

    Moved main loop into function to speed up separate processes

--- main.js
+++ main.js
@@ -10,134 +10,137 @@ const sitesJson = require(config.website_file);
 let getUrls = () => {
   // Main website data
   let urls = [];
-  // Keeps track of last time each link was opened
-  let lastOpened = {};
 
   for (let val of sitesJson["websites"]) {
-    // URL used for finding webpage
+    // Regex for parsing domain name
     const httpsPrefix = /^(https:\/\/)/;
     const httpPrefix = /^(http:\/\/)/;
-    let fullUrl, url;
+    let url, domain_name;
 
     if (config.debug)
       console.log("Processing URL " + val + "...");
 
-    // Adjust link depending on it it contains the http/https prefix
+    // Formatting link depending on if it contains the http/https prefix
     if (httpsPrefix.test(val)) {
-      url = val.substring(8);
-      fullUrl = val;
-    } else if (httpPrefix.test(val)) {
-      url = val.substring(7);
-      fullUrl = val;
-    } else {
+      domain_name = val.substring(8);
       url = val;
-      fullUrl = "https://" + val;
+    } else if (httpPrefix.test(val)) {
+      domain_name = val.substring(7);
+      url = val;
+    } else {
+      domain_name = val;
+      url = "https://" + val;
     }
 
-    let endOfDomain = url.indexOf("/");
+    // Trimming domain name
+    let endOfDomain = domain_name.indexOf("/");
     if (endOfDomain != -1)
-      url = url.substring(0, endOfDomain);
-
+      domain_name = domain_name.substring(0, endOfDomain);
 
     // Get domain name from url to check against tracked sites
-    let domain = psl.get(url);
+    let domain = psl.get(domain_name);
     if (config.debug)
-      console.log("Got " + domain + " from " + url);
+      console.log("Got " + domain + " from " + domain_name);
 
     // Check if domain name found in tracked sites
     if (trackedSites.hasOwnProperty(domain)) {
       urls.push({ 
-        "url" : fullUrl,
+        "url" : url,
         "element" : trackedSites[domain]
       });
-
-      lastOpened[fullUrl] = 0;
     } else {
       if (config.debug)
-        console.log("URL " + url + " not found in list of tracked URLs");
+        console.log("URL " + domain_name + " not found in list of tracked URLs");
     }
   };
 
-  if (config.debug) {
+  if (config.debug)
     console.log("urls: " + urls);
-    console.log("lastOpened: " + lastOpened);
-  }
-  return [urls, lastOpened];
+  return urls;
 };
 
 // Try and find the available button on the webpage and return true if clickable
 let checkPage = async (browser, url, element) => {
+  // Page refresh timeout
+  let lastOpened = 0;
+
+  // Page load options set in config file
+  const load_options = {
+    timeout   : config.load_timeout,
+    waitUntil : config.waitUntil
+  };
+
+  // Open a new tab and load the URL
   const page = await browser.newPage();
   if (config.debug)
     console.log("Initialising page " + url + "...");
-  await page.goto(url, { 
-    timeout   : 0,
-    waitUntil : 'networkidle0'
-  });
+  await page.goto(url, load_options);
   
-  let check = await page.evaluate((element) => {
-    // Check if availability element is present on screen
-    let el = document.querySelector(element); 
-    return (el != null);
-  }, element);
+  while (true) {
+    let check = await page.evaluate((element) => {
+      // Check if availability element is present on screen
+      let el = document.querySelector(element); 
+      return (el != null);
+    }, element);
 
-  if (config.debug)
-    console.log("Trying to find element " + element + "..." + (check ? "Success" : "Failed"));
+    if (config.debug)
+      console.log("Trying to find element " + element + "..." + (check ? "Success" : "Failed"));
 
+    // If buy button is available
+    if (check) {
+      let currentTime = new Date().getTime();
+      // Check page was last opened MORE than 30 seconds ago
+      if (currentTime - lastOpened > config.page_timeout) {
+        open(url);
+        // Update most recent page open time
+        lastOpened = currentTime;
+      } else {
+        if (config.debug) {
+          console.log(site.value + " last opened: " + currentTime - lastOpened[site.value] + "ms ago");
+        }
+      }
+    }
+
+    if (config.debug)
+      console.log("Reloading page " + url + "...");
+
+    // Reload the page and pray again
+    await page.reload(load_options);
+  }
+
+  // Shouldn't get here
   page.close();
-
-  if (check)
-    return Promise.resolve(url);
-  else
-    return Promise.reject(url);
+  return Promise.resolve(url);
 };
 
-//Main
+// Main
 (async () => {
   // Convert website JSON list to array
-  let [sites, lastOpened] = getUrls();
+  let sites = getUrls();
 
+  // Check that there is at least one array to work with
   if (sites === undefined || sites.length === 0) {
     console.log("Enter at least one valid website address");
     return;
   }
 
-  // Create new browser instance
+  // Set browser options according to config file
+  let launch_options = {};
   if (config.debug) {
-    var browser = await puppeter.launch({ 
+    launch_options = {
       headless  : config.headless,
       slowMo    : config.slowMo
-    });
-  } else {
-    var browser = await puppeter.launch();
+    };
   }
 
-  while (true) {
-    await Promise.all(sites.map(x => {
-      return checkPage(browser, x.url, x.element);
-    })).then((promises) => {
-      if (config.debug)
-        console.log(promises);
+  // Create browser instance
+  const browser = await puppeter.launch(launch_options);
 
-      promises.forEach((site) => {
-        if (site.status === "fulfilled") {
-          let currentTime = new Date().getTime();
-          // Check page was last opened MORE than 30 seconds ago
-          if (currentTime - lastOpened[site.value] > config.page_timeout) {
-            open(site.value);
-            // Refresh time of last time page was opened
-            lastOpened[site.value] = currentTime;
-          } else {
-            if (config.debug) {
-              console.log(site.value + " last opened: " + currentTime - lastOpened[site.value] + "ms ago");
-            }
-          }
-        }
-      })
-    })
-    .catch((promises) => { console.log(promises); })
-  };
+  // Start all the page checks
+  await Promise.all(sites.map(x => {
+    return checkPage(browser, x.url, x.element);
+  }));
 
-  // It shouldn't get here
+  // Shouldn't get here
   await browser.close();
 })();
